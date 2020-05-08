c8510a9e0a173f85a51a40129b7af7af34bedc2b
run-after <run.after@protonmail.com>
2020-05-08 13:28:36 -0500

    complete - no styles

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:content)
+    end
+end
